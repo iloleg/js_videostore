0d94f429b7f34a6d417c053eb38505d62bd5bd4a
Oleg <il.oleg@outlook.com>
2016-12-25 18:10:24 +0200

    rename totalFrequentRenterPoints

--- store.js
+++ store.js
@@ -2,7 +2,7 @@
 
 function statement(customer, movies) {
     let totalAmount = 0;
-    let frequentRenterPoints = 0;
+    let totalFrequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
     for (let r of customer.rentals) {
         let movie = movies[r.movieID]
@@ -28,17 +28,20 @@ function statement(customer, movies) {
         }
 
         //add frequent renter points
-        frequentRenterPoints++;
+        totalFrequentRenterPoints++;
         // add bonus for a two day new release rental
-        if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+        if (movie.code === "new" && r.days > 2) totalFrequentRenterPoints++;
 
         //print figures for this rental
         result += `\t${movie.title}\t${thisAmount}\n`;
         totalAmount += thisAmount;
     }
+
+
+
     // add footer lines
     result += `Amount owed is ${totalAmount}\n`;
-    result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+    result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
 
     return result;
 }
