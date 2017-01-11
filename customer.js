d81d7b01b68fed1824a6ae303fff5faa5d9ccca3
Oleg <il.oleg@outlook.com>
2017-01-11 06:38:46 +0200

    created class rental

--- customer.js
+++ customer.js
@@ -9,7 +9,19 @@ class Customer{
         return this._data.name;
     }
     get rentals(){
-        return this._data.rentals;
+        return this._data.rentals
+            .map(rental => new Rental(rental));
+    }
+}
+class Rental {
+    constructor(data){
+        this._data = data;
+    }
+    get movieID(){
+        return this._data.movieID;
+    }
+    get days(){
+        return this._data.days;
     }
 }
 module.exports = Customer;
